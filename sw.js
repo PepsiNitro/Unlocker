85c0d207f1791b2f55d8cb719a66b38e9f81cb69
Pepsi_Man <doomguyrisenow@gmail.com>
2023-03-01 15:20:30 +0000

    please

--- sw.js
+++ sw.js
@@ -1,48 +1,25 @@
-var cacheName = 'orchache';
-var cacheAssets = [
-	'/',
+var cacheName = 'hello-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/js/main.js'
 ];
 
-// Call install Event
-self.addEventListener('install', e => {
-	// Wait until promise is finished
-	e.waitUntil(
-		caches.open(cacheName)
-		.then(cache => {
-			console.log(`Service Worker: Caching Files: ${cache}`);
-			cache.addAll(cacheAssets)
-				// When everything is set
-				.then(() => self.skipWaiting())
-		})
-	);
-})
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
 
-
-
-
-
-
-// Call Fetch Event
-self.addEventListener('fetch', e => {
-	console.log('Service Worker: Fetching');
-	e.respondWith(
-		fetch(e.request)
-		.then(res => {
-			// The response is a stream and in order the browser
-			// to consume the response and in the same time the
-			// cache consuming the response it needs to be
-			// cloned in order to have two streams.
-			const resClone = res.clone();
-			// Open cache
-			caches.open(cacheName)
-				.then(cache => {
-					// Add response to cache
-					cache.put(e.request, resClone);
-				});
-			return res;
-		}).catch(
-			err => caches.match(e.request)
-			.then(res => res)
-		)
-	);
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
